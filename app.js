ea8cda78f6b3f8ed79e05041aad0865604a35eb2
Adhish Bahl <adhishbahl0@gmail.com>
2021-05-14 20:05:39 +0530

    Javascript done

--- app.js
+++ app.js
@@ -8,15 +8,32 @@
 // alert("Welcome " + promptval);
 
 // Exercise 3
-var transbtn = document.querySelector("#transbtn");
 // transbtn.addEventListener("click", printa);
 
 // function printa() {
-//     console.log("Clicked!");
-// }
-
+    //     console.log("Clicked!");
+    // }
+    
 var input = document.querySelector("#input");
-transbtn.addEventListener("click", printb);
-function printb() {
-    console.log(input.value);
+var output = document.querySelector("#output");
+var transbtn = document.querySelector("#transbtn");
+var url = "https://api.funtranslations.com/translate/minion.json?text=";
+
+function errorHandler() {
+    alert("The server is not responding, kindly try again later!")
 }
+
+function printb() {
+    var inputvalue = input.value;
+    fetch(url+inputvalue)
+    .then(response => response.json())
+    // .then(json => console.log(json.contents.text))
+    .then(json => {
+        var outputtext = json.contents.translated;
+        output.innerText = outputtext;
+    })
+    .catch(errorHandler)
+    // output.innerText = inputvalue;
+}
+
+transbtn.addEventListener("click", printb);
